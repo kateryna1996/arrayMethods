2952a9f069c815a5a0822e6126373e298269dbcd
kateryna1996 <k.levchenko28@gmail.com>
2022-03-31 14:56:22 +0200

    part one

--- arrayMethods.js
+++ arrayMethods.js
@@ -19,18 +19,27 @@ const inventors = [
 // Verwachte uitkomst:
 // [{ first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 }, { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }]
 
+const filteredInventors = inventors.filter((inventor) =>{
+  return inventor.year < 1600 && inventor.year > 1500;
+})
+console.log(filteredInventors);
 
 
 // 2. Maak een array met daarin alle geboortejaren van de uitvinders
 // Verwachte uitkomst: [1879, 1643, 1564, 1867, 1571, 1473, 1858, 1898, 1815, 1855, 1878, 1847];
 
-
+const  yearOfBirth = inventors.map((inventor) =>{
+ return  inventor.year;
+});
+console.log(yearOfBirth);
 
 // 3. Maak een array met daarin alle volledige namen van de uitvinders (dus voor- en achternaam als één string)
 // Verwachte uitkomst: [ 'Albert Einstein', 'Isaac Newton', 'Galileo Galilei', 'Marie Curie', 'Johannes Kepler', 'Nicolaus Copernicus', 'Max Planck', 'Katherine Blodgett', 'Ada Lovelace', 'Sarah E. Goode', 'Lise Meitner', 'Thomas Edison']
+const fullName = inventors.map((inventor) =>{
+  return inventor.first + " " + inventor.last;
+});
 
-
-
+console.log(fullName);
 // 4. Sorteer de uitvinders op geboortejaar, oplopend van oudste naar jongste uitvinder
 // Verwachte uitkomst:
 // [
